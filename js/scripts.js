4287a7c2d56eb170f73bee71d92c43c0480be8d8
AdamManselll <adammansellhmb@gmail.com>
2020-08-20 12:05:43 -0700

    Taking break for lunch.
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,35 @@ $(document).ready(function () {
   //   const userInput = parseInt($("#input").val());
   // })
 
-  console.log(checkInput(0));
-
   function checkInput(input) {
     if (input > 3999 || input <= 0) {
       alert("Please enter a valid number. Valid numbers are 1 to 3999.")
       return false;
     }
     else {
-      valueAdd(input);
+      input = input.toString();
+      return valueAdd(input);
     }
   }
 
+  function valueAdd(input) {
+    let symbolArray = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    let valueArray = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
 
-
-
+    for (i = 0; i < symbolArray.length; i++) {
+      if (input >= valueArray[i] || input < valueArray[i+1]) {
+        return symbolArray[i];
+      }
+    }
+  }
+  console.log(checkInput(10));
 })
+
+
+
+
+
+
+
+
+
